1efe9c6e2e1208d4ee92b5d58108b18f4eb2aa10
Till Hoffmann <tillahoffmann@gmail.com>
2022-03-15 19:14:31 -0400

    Fix stripping of sphinx directives for PyPI.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 with open("README.rst") as fp:
     long_description = fp.read()
 long_description = long_description.replace(".. doctest::", ".. code-block:: python")
-long_description = re.sub(":(math)|(func)|(class):", ":code:", long_description)
+long_description = re.sub(r":((math)|(func)|(class)):", ":code:", long_description)
 long_description = re.sub(
     r"((\.\. automodule:: .*?$)|(\.\. toctree::)|(\.\. plot:: .*?$))", r".. code-block::\n\n  \1",
     long_description, flags=re.MULTILINE
